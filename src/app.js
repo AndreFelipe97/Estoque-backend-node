c7ab3384c3e01e1395cf6b506fe643a12496d344
andre <andre@layoutsistemas.com.br>
2020-05-12 19:02:36 -0300

    Foi configurado o cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import routes from './routes';
 import './database';
 
@@ -11,6 +12,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors({ origin: 'http://localhost:3000' }));
     this.server.use(express.json());
     this.server.use('/files', express.static(path.resolve(__dirname, '..', 'temp', 'uploads')));
   }
